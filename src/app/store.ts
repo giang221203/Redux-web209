0cb7623528fc2445eccb13435fee4cfa44449867
Your Name <you@example.com>
2023-07-20 10:59:46 +0700

    redux-thunk

--- src/app/store.ts
+++ src/app/store.ts
@@ -1,11 +1,12 @@
-import { legacy_createStore as createStore, combineReducers } from 'redux';
+import { legacy_createStore as createStore, combineReducers ,applyMiddleware} from 'redux';
 import { counterReducer } from '@/reducers/Counter';
 import { productReducer } from '@/reducers/Product';
+import thunk from 'redux-thunk'
 
 const rootReducer = combineReducers({
     counter: counterReducer,
     products: productReducer
 })
 
-const store = createStore(rootReducer);
+const store = createStore(rootReducer,applyMiddleware(thunk));
 export default store;
